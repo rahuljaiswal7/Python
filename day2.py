48c935a848bebc81d80cfa47d619ece3068efec6
Rahul Jaiswal <157681484+rahuljaiswal7@users.noreply.github.com>
2025-04-01 22:35:51 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,22 @@
+from operator import truediv
+
+print("Hello Guys")
+# Escape
+print("Hello Guys \n Welcome to My program")
+print(9,6)
+print("Hello Rahul",6,7, sep="~", end="007")
+
+# variable
+a =10
+b =20
+c ="Relax Boys"
+d = True
+e =3.14
+li = [1,3,["apple"]]
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(li))
+
