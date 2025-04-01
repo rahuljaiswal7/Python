f8b1308f583afa1866d2a4225bc1786d02c2b0d7
Rahul Jaiswal <157681484+rahuljaiswal7@users.noreply.github.com>
2025-04-01 22:34:45 +0530

    Add files via upload

---
+++ New.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print("Hello World")
+# print("Hello World") comment for single line  alt+/ or multiple line ctrl+/
+
+a = 10
+b = 20
+sum = a + b
+print(sum)
+# print(a + b)
+# print(a - b)
+# print(a * b)
+a = 10
+print(a)
+# print( "the variable value", a)
