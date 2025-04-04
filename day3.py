499fc21b9a3b03d9e1b74b7727477fcbe208b2cb
Rahul Jaiswal <157681484+rahuljaiswal7@users.noreply.github.com>
2025-04-04 06:35:36 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+print("The value of a+b =",a+b)
+print("The value of a-b =",a-b)
+print("The value of a*b =",a*b)
+print("The value of a/b =",a/b)
+print("The value of a//b =",a//b)
+print("The value of a%b =",a%b)
+print("The value of a**b =",a**b)
+
+
+
+
