f36471a38cafca62f96ee0df16cebc6e0a2edd80
Rahul Jaiswal <157681484+rahuljaiswal7@users.noreply.github.com>
2025-04-04 06:45:10 +0530

    Add files via upload

---
+++ Slicer.py
@@ -0,0 +1,52 @@
+Name ="RAHUL KUMAR JAISWAL"
+print(Name[0:8:1])
+print(Name[:8:2])
+
+# Negative Slicer
+print(Name[10::-1])
+
+# Q1
+txt = "PythonProgram"
+print(txt[2:8])
+
+# Q2
+text = "Hello World!"
+print(text[:5])
+
+# Q3
+rev = "Hello Guys"
+print(rev[::-1])
+
+# Q4
+Aplphabet = "ABCDEFG"
+print(Aplphabet[::2])
+
+# Q5
+msg = "Python"
+print(msg[1:-1])
+
+# Q6
+MSG ="Programming"
+print(MSG[5:1:-1])
+
+# Q7
+techincal ="Data Science"
+print(techincal[-5:])
+# print(techincal[::0])
+
+text1 = "abcdefg"
+print(text1[1:6:2])
+
+
+N ="RAHUL"
+print(len(N))
+# print(N[-3:-1])
+# print(N[0:-4])
+print(N[0:-1])
+print(N[-4:-2])
+
+XY = "Hello Guys Welcome To Slicer Class"
+print(len(XY))
+# print(XY[0:18:2])
+print(XY[-25:-5:2])
+
