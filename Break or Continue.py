e9e4a7dbbdbc84d9807a30ff2d83c02b2d08f7af
Rahul Jaiswal <157681484+rahuljaiswal7@users.noreply.github.com>
2025-04-10 19:05:47 +0530

    Add files via upload

---
+++ Break or Continue.py
@@ -0,0 +1,29 @@
+
+# num1 = int(input("Enter Your Number"))
+
+# for i in range(1,10):
+#     if i == 5:
+#      break
+#     print(i)
+
+# for i in range(12):
+#     if i == 11:
+#         break
+#     print("5 X",i,"=",5*i)
+# print("Mujhe Order Mila Ki Bahar Jao")
+
+# continue Statement
+# for i in range(1,21):
+#     if i == 11:
+#         continue
+#     print("5 X",i, "=",5*i)
+# print("mujhe Bola Gya Ki 55 ko Skip Kr Do")
+
+# Break with dowhile loop
+i = 0
+while True:
+    print(i)
+    i = i + 1
+    if(i%100 == 0):
+        break
+print("Mai Loop Ko Band Kiya")
